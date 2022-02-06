04037a698435fd74cc59da864ecd5da1656e6ea9
Koray Liman <69529444+KorayLiman@users.noreply.github.com>
2022-02-06 14:40:41 +0300

    wd

---
+++ lib/burc_details_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BurcDetail extends StatelessWidget {
+  const BurcDetail({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
